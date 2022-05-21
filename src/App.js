8f9d8d053d6512c4ced796dd29b299af0a66ab05
jengoetz <jennifer.goetz@koho.ca>
2022-05-21 12:14:43 -0400

    start to add coinbase integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-import Transak from './Transak.jsx'
+import "./App.css";
+import Transak from "./Transak.jsx";
+import { useWeb3React } from "@web3-react/core";
+import { connectors } from "./connectors";
 
 function App() {
+  const { activate, deactivate } = useWeb3React();
+
   return (
     <div className="App">
       hi
-      <Transak/>
+      <Transak />
+      <button
+        onClick={() => {
+          activate(connectors.coinbaseWallet);
+        }}
+      >
+        Coinbase Wallet
+      </button>
+      <button
+        onClick={() => {
+          activate(connectors.walletConnect);
+        }}
+      >
+        Wallet Connect
+      </button>
+      <button
+        onClick={() => {
+          activate(connectors.injected);
+        }}
+      >
+        Metamask
+      </button>
+      <button onClick={deactivate}>Disconnect</button>
     </div>
   );
 }
---
+++ src/connectors.js
@@ -0,0 +1,27 @@
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+const key = "17d97eeb779e4a09865898f0513c904c";
+
+const coinbaseWallet = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${key}`,
+  appName: "Web3-react Demo",
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const walletConnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/${key}`,
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+});
+
+const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+export const connectors = {
+  injected,
+  walletConnect,
+  coinbaseWallet,
+};
