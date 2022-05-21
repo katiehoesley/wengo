902a593cfd6c8b7d89754e17c707806fcb349c4a
katiehoesley <katiehoesley@gmail.com>
2022-05-21 18:05:21 -0400

    metamask success

--- src/Coinbase.jsx
+++ src/Coinbase.jsx
@@ -5,13 +5,16 @@ import Button from '@mui/material/Button';
 function Coinbase () {
     const { activate, deactivate, active, chainId, account } = useWeb3React();
     const imgSize = 26;
-    <>
+    
+    return(
+    <div>
         <Button
             onClick={() => {
                 activate(connectors.coinbaseWallet);
             } }
         >
         <img
+            alt=""
             src="https://demo.storj-ipfs.com/ipfs/QmaaWoX3our4L3BK7YZM4ihvP1MdKhjaLSpaHb1QkZie58"
             height={imgSize}
             width={imgSize}
@@ -23,29 +26,37 @@ function Coinbase () {
                 } }
             >
             <img
+                alt=""
                 src="https://demo.storj-ipfs.com/ipfs/QmWurqmXGPSoTQqwH7iDSGJuiXKoPA8n1f8kpZdX9hYoKQ"
                 height={imgSize}
                 width={imgSize} />
                 Wallet Connect
-            </Button><Button
+            </Button>
+            
+            <Button
                 onClick={() => {
                     activate(connectors.injected);
                 } }
             >
             <img
+                alt=""
                 src="https://demo.storj-ipfs.com/ipfs/Qmboo7p1D7oWaPhtKgugiavL9JrQ7Q24c1pU4xPTdmSKgZ"
                 height={imgSize}
                 width={imgSize} />
-            Metamask
-        </Button></>
+                Metamask
+            </Button>
+        
+
             {active && <button onClick={deactivate}>Disconnect</button>}
             <div>Connection Status: {active ? "connected!" : "not connected"}</div>
             {active && (
-        <>
-            <div>Account: {account}</div>
-            <div>Network ID: {chainId}</div>
-        </>
-  )}
-}
+                <>
+                    <div>Account: {account}</div>
+                    <div>Network ID: {chainId}</div>
+                </>
+            )}
+
+        </div>
+)}
 
 export default Coinbase
