e6fb29230465f21ca5330467460b8f1ac836a910
katiehoesley <katiehoesley@gmail.com>
2022-05-21 17:46:01 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,79 +1,20 @@
 
-//import "./App.css";
-import { useWeb3React } from "@web3-react/core";
-import { connectors } from "./connectors.js";
-import meta from "./pics/mm.png";
-import coinbase from "./pics/cbw.png";
-import wallet from "./pics/wc.png";
 import NavBar from "./NavBar.jsx";
 import Home from "./Home.jsx";
 import "./styles.css";
 
-import NavBar from './NavBar.jsx'
-import Home from './Home.jsx'
-import './styles.css'
-import UserPage from './UserPage.jsx'
-import About from './About.jsx'
-
 
 function App() {
-  const { activate, deactivate, active, chainId, account } = useWeb3React();
-  const imgSize = 26;
 
   return (
 
     <div className="App">
       <NavBar />
       <Home />
-      <button
-        onClick={() => {
-          activate(connectors.coinbaseWallet);
-        }}
-      >
-        <img
-          src={coinbase}
-          height={imgSize}
-          width={imgSize}
-          borderRadius="50%"
-        />
-        Coinbase Wallet
-      </button>
-      <button
-        onClick={() => {
-          activate(connectors.walletConnect);
-        }}
-      >
-        <img src={wallet} height={imgSize} width={imgSize} />
-        Wallet Connect
-      </button>
-      <button
-        onClick={() => {
-          activate(connectors.injected);
-        }}
-      >
-        <img src={meta} height={imgSize} width={imgSize} />
-        Metamask
-      </button>
-      {active && <button onClick={deactivate}>Disconnect</button>}
-      <div>Connection Status: {active ? "connected!" : "not connected"}</div>
-      {active && (
-        <>
-          <div>Account: {account}</div>
-          <div>Network ID: {chainId}</div>
-        </>
-      )}
     </div>
   );
 }
 
-    <div className="app">
-        <NavBar/>
-        <Home/>
-        <UserPage/>
-        <About />
-    </div>
-    )
-  }
 
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import NFTCollection from './NFTCollection.jsx'
+import Coinbase from './Coinbase.jsx'
 
 function Home() {
     return(
         <div>
             <h1>Welcome to Whatever</h1>
             <h2>To get started, connect using your Coinbase Wallet</h2>
+            <Coinbase />
             <NFTCollection/>
         </div>
     )
