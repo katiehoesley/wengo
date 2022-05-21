8b6ef74b95e98a41f4408343dd9bc4210fc94b61
katiehoesley <katiehoesley@gmail.com>
2022-05-21 10:53:54 -0400

    gets rid of weird ass coinbase stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 import Transak from './Transak.jsx'
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { InjectedConnector } from "@web3-react/injected-connector";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Web3ReactProvider } from '@web3-react/core'
-import { Web3Provider } from "@ethersproject/providers";
 
-function getLibrary(provider) {
-  return new Web3Provider(provider);
-}
-
-ReactDOM.render(
-  <Web3ReactProvider getLibrary={getLibrary}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Web3ReactProvider>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
