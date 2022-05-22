df9b02dd2de6345b5f313e90a0580e1b914dbcbe
katiehoesley <katiehoesley@gmail.com>
2022-05-22 02:56:03 -0400

    update

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -5,11 +5,12 @@ function SignUp() {
         <div id="get-started">
             <h1 id="title">wengo events!</h1>
                 <div id="homepage-text">
-                    <h1>what's comin' up?</h1>
-                    <h4>the wengo marathon is coming up! register for 3 $matic!</h4>
+                    <h1>the wengo marathon is coming up!</h1>
+                    <h4>register for 3 $matic!</h4>
+                    <img id="marathon" src="https://demo.storj-ipfs.com/ipfs/QmRZdmuoesyqrBYoH1hsX7CXMJZeFvHT5vg5pRoWbF6dDP"/>
+                    <h4>to add $matic to your account, use transak!</h4>
+                    <Transak />
                 </div>
-            <h4>to add $matic to your account, use transak!</h4>
-            <Transak />
         </div>
     )
 }
